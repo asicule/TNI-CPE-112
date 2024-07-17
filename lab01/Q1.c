7cd7fa371a06ea4fcb513068106057e8d7da8d8a
Phakin Kongkha <secured.ntrain@gmail.com>
2024-07-17 14:10:24 +0700

    lab01

---
+++ lab01/Q1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main()
+{
+	printf("Hello my world!\n");
+	printf("MAGIC!\n");
+	return 0;
+}
---
+++ lab01/Q2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main()
+{
+	printf("MAGIC! ");
+	printf("Hello my world!\n");
+	return 0;
+}
