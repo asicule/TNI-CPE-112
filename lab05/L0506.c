eb273a96bb5c9784336dbf4c5b87c89df296ff69
Phakin Kongkha <secured.ntrain@gmail.com>
2024-07-31 17:29:13 +0700

    lab05/L0506 use goto

--- lab05/L0506.c
+++ lab05/L0506.c
@@ -8,11 +8,12 @@ main(void)
 	fputs("Enter the number for factorial: ", stdout);
 	scanf("%d", &factorial);
 	i = factorial;
-	while (i > 0) {
-		sum *= i;
-		printf("%ld ", i--);
-		if (i) fputs("* ", stdout);
-		else break;
+addsum:
+	sum *= i;
+	printf("%ld ", i--);
+	if (i) {
+		fputs("* ", stdout);
+		goto addsum;
 	}
 
 	printf("= %ld\n", sum);
